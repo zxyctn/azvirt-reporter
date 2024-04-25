1d7961f5ca965564c766ece7bc54c99b7f691e01
Ibrahim Mammadov <zxyctn@gmail.com>
2024-04-26 01:30:22 +0200

    Add accordions

--- src/routes/Calc.tsx
+++ src/routes/Calc.tsx
@@ -1,8 +1,16 @@
 import { useState } from 'react';
 
 import CalcInput from '@/lib/components/CalcInput';
+import Parameter from '@/lib/components/Parameter';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 import { Dialog, DialogContent } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
+import { Badge } from '@/components/ui/badge';
 
 const Calc = () => {
   const [open, setOpen] = useState(false);
@@ -20,6 +28,55 @@ const Calc = () => {
           <Button className='w-full font-bold'>CALC</Button>
         </DialogContent>
       </Dialog>
+
+      <div className='p-3 sm:px-0'>
+        <div className='flex gap-3 overflow-auto'>
+          <Badge variant='secondary' className='rounded-full cursor-pointer'>
+            0/31
+          </Badge>
+          <Badge className='rounded-full cursor-pointer'>BNS32</Badge>
+          <Badge variant='secondary' className='rounded-full cursor-pointer'>
+            BNS22
+          </Badge>
+          <Badge variant='secondary' className='rounded-full cursor-pointer'>
+            SMA
+          </Badge>
+        </div>
+        <Accordion type='multiple' className='w-full'>
+          <AccordionItem value='item-1'>
+            <AccordionTrigger>General</AccordionTrigger>
+            <AccordionContent>
+              <div className='grid grid-cols-2 sm:grid-cols-4 gap-3 md:gap-4 lg:gap-5'>
+                <Parameter />
+                <Parameter />
+                <Parameter />
+                <Parameter readOnly />
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value='item-2'>
+            <AccordionTrigger>Bitumen</AccordionTrigger>
+            <AccordionContent>
+              <div className='grid grid-cols-2 gap-3 md:gap-4 lg:gap-5'>
+                <Parameter />
+                <Parameter readOnly />
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value='item-3'>
+            <AccordionTrigger>Limestone</AccordionTrigger>
+            <AccordionContent>
+              <div className='grid grid-cols-2 sm:grid-cols-3 gap-3 md:gap-4 lg:gap-5'>
+                <Parameter />
+                <Parameter />
+                <div className='col-span-2 sm:col-span-1'>
+                  <Parameter readOnly />
+                </div>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </div>
   );
 };
