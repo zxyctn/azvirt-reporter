99a84dda5438e298123347428000f9397214e0f4
Ibrahim Mammadov <zxyctn@gmail.com>
2024-04-28 13:17:25 +0200

    Create MobX stores

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,7 +1,15 @@
+import { v4 as uuidv4 } from 'uuid';
 import { makeAutoObservable } from 'mobx';
 import { SupabaseClient, createClient } from '@supabase/supabase-js';
 
-import type { Theme } from '@/lib/types';
+import type {
+  BNS22,
+  BNS32,
+  Base,
+  SMA,
+  Theme,
+  Calculation as CalculationType,
+} from '@/lib/types';
 
 class ThemeStore {
   theme: Theme = 'light';
@@ -43,6 +51,327 @@ class SupabaseStore {
   }
 }
 
+class Defaults {
+  type: 'length' | 'weight' = 'length';
+  unit: 'm' | 't' = 'm';
+  Base: Base = {
+    general: {
+      thickness: {
+        value: 0.1,
+        unit: '',
+      },
+      width: {
+        value: 8.5,
+        unit: '',
+      },
+      bulkDensity: {
+        value: 2.2,
+        unit: '',
+      },
+    },
+  };
+  BNS32: BNS32 = {
+    general: {
+      thickness: {
+        value: 0.09,
+        unit: '',
+      },
+      width: {
+        value: 8.2,
+        unit: '',
+      },
+      bulkDensity: {
+        value: 2.4,
+        unit: '',
+      },
+    },
+    bitumen: {
+      fraction: {
+        value: 0.038,
+        unit: '',
+      },
+    },
+    limestone: [
+      {
+        thickness: {
+          value: '0-4',
+          unit: 'mm',
+          disabled: true,
+        },
+        percentage: {
+          value: 40,
+          unit: '%',
+        },
+      },
+      {
+        thickness: {
+          value: '4-8',
+          unit: 'mm',
+          disabled: true,
+        },
+        percentage: {
+          value: 5,
+          unit: '%',
+        },
+      },
+      {
+        thickness: {
+          value: '8-16',
+          unit: 'mm',
+          disabled: true,
+        },
+        percentage: {
+          value: 30,
+          unit: '%',
+        },
+      },
+      {
+        thickness: {
+          value: '16-32',
+          unit: 'mm',
+          disabled: true,
+        },
+        percentage: {
+          value: 30,
+          unit: '%',
+        },
+      },
+    ],
+  };
+  BNS22: BNS22 = {
+    general: {
+      thickness: {
+        value: 0.07,
+        unit: '',
+      },
+      width: {
+        value: 8.1,
+        unit: '',
+      },
+      bulkDensity: {
+        value: 2.4,
+        unit: '',
+      },
+    },
+    bitumen: {
+      fraction: {
+        value: 0.041,
+        unit: '',
+      },
+    },
+    limestone: [
+      {
+        thickness: {
+          value: '0-4',
+          unit: 'mm',
+          disabled: true,
+        },
+        percentage: {
+          value: 40,
+          unit: '%',
+        },
+      },
+      {
+        thickness: {
+          value: '4-8',
+          unit: 'mm',
+          disabled: true,
+        },
+        percentage: {
+          value: 10,
+          unit: '%',
+        },
+      },
+      {
+        thickness: {
+          value: '8-16',
+          unit: 'mm',
+          disabled: true,
+        },
+        percentage: {
+          value: 20,
+          unit: '%',
+        },
+      },
+      {
+        thickness: {
+          value: '16-22',
+          unit: 'mm',
+          disabled: true,
+        },
+        percentage: {
+          value: 30,
+          unit: '%',
+        },
+      },
+    ],
+  };
+  SMA: SMA = {
+    general: {
+      thickness: {
+        value: 0.04,
+        unit: '',
+      },
+      width: {
+        value: 8,
+        unit: '',
+      },
+      bulkDensity: {
+        value: 2.25,
+        unit: '',
+      },
+    },
+    bitumen: {
+      fraction: {
+        value: 0.064,
+        unit: '',
+      },
+    },
+  };
+
+  constructor(defaults: Defaults | null = null) {
+    if (defaults) {
+      this.type = defaults.type;
+      this.unit = defaults.unit;
+      this.Base = defaults.Base;
+      this.BNS32 = defaults.BNS32;
+      this.BNS22 = defaults.BNS22;
+      this.SMA = defaults.SMA;
+    }
+    makeAutoObservable(this);
+  }
+
+  setType(type: 'length' | 'weight') {
+    this.type = type;
+    this.unit = type === 'length' ? 'm' : 't';
+  }
+
+  setParameter(
+    layer: 'Base' | 'BNS32' | 'BNS22' | 'SMA',
+    group: 'general' | 'bitumen' | 'limestone',
+    parameter: 'thickness' | 'width' | 'bulkDensity' | 'fraction',
+    value: number
+  ) {
+    if (
+      group in this[layer] &&
+      // @ts-ignore
+      parameter in this[layer][group] &&
+      // @ts-ignore
+      'value' in this[layer][group][parameter]
+    ) {
+      // @ts-ignore
+      this[layer][group][parameter].value = value;
+    } else {
+      console.error('Invalid parameter');
+    }
+  }
+
+  setLimestonePercentage(
+    layer: 'BNS32' | 'BNS22',
+    index: number,
+    value: number
+  ) {
+    if (index < this[layer].limestone.length) {
+      this[layer].limestone[index].percentage.value = value;
+    } else {
+      console.error('Invalid index');
+    }
+  }
+}
+
+class Calculation {
+  createdAt: Date;
+  updatedAt: Date;
+  parameters: Defaults;
+  value: number = 0;
+
+  constructor() {
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+    this.parameters = new Defaults(defaults);
+    makeAutoObservable(this);
+  }
+
+  update() {
+    this.updatedAt = new Date();
+  }
+
+  totalWeight(layer: 'Base' | 'BNS32' | 'BNS22' | 'SMA') {
+    return (
+      this.parameters[layer].general.width.value *
+      this.parameters[layer].general.bulkDensity.value *
+      this.parameters[layer].general.thickness.value *
+      this.value
+    );
+  }
+
+  bitumenWeight(layer: 'BNS32' | 'BNS22' | 'SMA') {
+    return (
+      this.totalWeight(layer) * this.parameters[layer].bitumen.fraction.value
+    );
+  }
+
+  limestoneWeight(layer: 'BNS32' | 'BNS22') {
+    return this.parameters[layer].limestone.map((limestone) => {
+      return (this.totalWeight(layer) * limestone.percentage.value) / 100;
+    });
+  }
+
+  get totalBaseWeight() {
+    return this.totalWeight('Base');
+  }
+
+  get totalBNS32Weight() {
+    return this.totalWeight('BNS32');
+  }
+
+  get totalBNS22Weight() {
+    return this.totalWeight('BNS22');
+  }
+
+  get totalSMAWeight() {
+    return this.totalWeight('SMA');
+  }
+
+  get totalBNS32BitumenWeight() {
+    return this.bitumenWeight('BNS32');
+  }
+
+  get totalBNS22BitumenWeight() {
+    return this.bitumenWeight('BNS22');
+  }
+
+  get totalSMABitumenWeight() {
+    return this.bitumenWeight('SMA');
+  }
+
+  get totalBNS32LimestoneWeight() {
+    return this.limestoneWeight('BNS32');
+  }
+
+  get totalBNS22LimestoneWeight() {
+    return this.limestoneWeight('BNS22');
+  }
+}
+
+class History {
+  calculations: CalculationType[] = [];
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  addCalculation(calculation: Calculation) {
+    this.calculations.push({
+      id: uuidv4(),
+      createdAt: calculation.createdAt.toISOString(),
+      updatedAt: calculation.updatedAt.toISOString(),
+      value: calculation.value,
+      parameters: calculation.parameters,
+    });
+  }
+}
+
 export const themeStore = new ThemeStore();
 export const supabaseStore = new SupabaseStore(
   createClient(
@@ -50,3 +379,6 @@ export const supabaseStore = new SupabaseStore(
     `${import.meta.env.VITE_SUPABASE_API_KEY}`
   )
 );
+export const defaults = new Defaults();
+export const calculation = new Calculation();
+export const history = new History();
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,22 +1,20 @@
 export type Theme = 'light' | 'dark' | 'system';
 
-export type Default = {
-  '0/31': Base;
+export type Defaults = {
+  Base: Base;
   BNS32: BNS32;
   BNS22: BNS22;
   SMA: SMA;
   type: 'length' | 'weight';
+  unit: 'm' | 't';
 };
 
 export type Calculation = {
   id: string;
   createdAt: string;
-  type: 'length' | 'weight';
+  updatedAt: string;
   value: number;
-  '0/31': Base;
-  BNS32: BNS32;
-  BNS22: BNS22;
-  SMA: SMA;
+  parameters: Defaults;
 };
 
 export type Base = {
@@ -26,111 +24,61 @@ export type Base = {
 export type BNS32 = {
   general: General;
   bitumen: Bitumen;
-  limestone: LimestoneBNS32;
+  limestone: Limestone[];
 };
 
 export type BNS22 = {
   general: General;
   bitumen: Bitumen;
-  limestone: LimestoneBNS22;
+  limestone: Limestone[];
 };
 
 export type SMA = {
   general: General;
+  bitumen: Bitumen;
+};
+
+export type Parameter = {
+  value: number;
+  unit: string;
 };
 
 export type General = {
-  thickness: number;
-  width: number;
-  bulkDensity: number;
+  thickness: Parameter;
+  width: Parameter;
+  bulkDensity: Parameter;
 };
 
 export type Bitumen = {
-  percentage: number;
+  fraction: Parameter;
 };
 
-export type LimestoneBNS22 = [
-  {
-    thickness: {
-      range: {
-        from: 0;
-        to: 4;
-      };
-      unit: 'mm';
-    };
-    percentage: 40;
-  },
-  {
-    thickness: {
-      range: {
-        from: 4;
-        to: 8;
-      };
-      unit: 'mm';
-    };
-    percentage: 10;
-  },
-  {
-    thickness: {
-      range: {
-        from: 8;
-        to: 16;
-      };
-      unit: 'mm';
-    };
-    percentage: 20;
-  },
-  {
-    thickness: {
-      range: {
-        from: 16;
-        to: 22;
-      };
-      unit: 'mm';
-    };
-    percentage: 30;
-  }
-];
+export type Limestone = {
+  thickness: {
+    value: string;
+    unit: string;
+    disabled: boolean;
+  };
+  percentage: Parameter;
+};
 
-export type LimestoneBNS32 = [
-  {
-    thickness: {
-      range: {
-        from: 0;
-        to: 4;
-      };
-      unit: 'mm';
-    };
-    percentage: 40;
-  },
-  {
-    thickness: {
-      range: {
-        from: 4;
-        to: 8;
-      };
-      unit: 'mm';
-    };
-    percentage: 5;
-  },
-  {
-    thickness: {
-      range: {
-        from: 8;
-        to: 16;
-      };
-      unit: 'mm';
-    };
-    percentage: 30;
-  },
-  {
-    thickness: {
-      range: {
-        from: 16;
-        to: 32;
-      };
-      unit: 'mm';
-    };
-    percentage: 30;
-  }
-];
+// Component props
+export type ParameterProps = {
+  readOnly?: boolean;
+  disabled?: boolean;
+  layer?: 'Base' | 'BNS32' | 'BNS22' | 'SMA';
+  group?: 'general' | 'bitumen' | 'limestone';
+  name:
+    | 'thickness'
+    | 'width'
+    | 'bulkDensity'
+    | 'fraction'
+    | 'weight'
+    | 'length'
+    | 'percentage';
+  unit?: string;
+  value?: number;
+  computed?: number;
+  parameters?: any[];
+  onChange?: (value: number, parameters: any[]) => void;
+};
