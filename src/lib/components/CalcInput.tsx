26a58e6be502ec776b16c7b40532f9a53e2377cb
Ibrahim Mammadov <zxyctn@gmail.com>
2024-04-28 13:17:40 +0200

    Connect to stores

--- src/lib/components/CalcInput.tsx
+++ src/lib/components/CalcInput.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { DotsVerticalIcon } from '@radix-ui/react-icons';
+
 import { Input } from '@/components/ui/input';
 import { Badge } from '@/components/ui/badge';
 import {
@@ -8,10 +10,16 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
-import { DotsVerticalIcon } from '@radix-ui/react-icons';
+import { calculation, history } from '@/lib/stores';
 
-const CalcInput = ({ readOnly = false }: { readOnly?: boolean }) => {
-  const [value, setValue] = useState<string>('');
+const CalcInput = ({
+  readOnly = false,
+  onClick,
+}: {
+  readOnly?: boolean;
+  onClick?: () => void;
+}) => {
+  const [value, setValue] = useState<string>(calculation.value.toString());
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const regex = /^[0-9.]*$/;
@@ -20,61 +28,78 @@ const CalcInput = ({ readOnly = false }: { readOnly?: boolean }) => {
     }
   };
 
+  const calculateHandler = () => {
+    calculation.value = parseFloat(value);
+    history.addCalculation(calculation);
+    onClick && onClick();
+  };
+
   return (
-    <div className='relative'>
-      <div className='absolute w-full top-3 flex justify-between h-min px-3'>
-        {/* Settings menu */}
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant='ghost' className='rounded-full p-0 m-0 w-6 h-6'>
-              <DotsVerticalIcon className='w-3 h-3' />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className='right:0 absolute left-auto top-full w-auto'>
-            <DropdownMenuItem>
-              <span className='text-xs'>Edit defaults</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <span className='text-xs'>History</span>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+    <>
+      <div className='relative'>
+        <div className='absolute w-full top-3 flex justify-between h-min px-3'>
+          {/* Settings menu */}
+          <div className='w-6'>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant='ghost'
+                  className='rounded-full p-0 m-0 w-6 h-6'
+                >
+                  <DotsVerticalIcon className='w-3 h-3' />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className='right:0 absolute left-auto top-full w-auto'>
+                <DropdownMenuItem>
+                  <span className='text-xs'>Edit defaults</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span className='text-xs'>History</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          {/* Input type menu */}
+          <div className='-mt-0.5'>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Badge className='cursor-pointer'>Length</Badge>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <span className='text-xs'>Length</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span className='text-xs'>Weight</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
 
-        {/* Input type menu */}
-        <div className='-mt-0.5'>
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Badge className='cursor-pointer'>Length</Badge>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem>
-                <span className='text-xs'>Length</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span className='text-xs'>Weight</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          {/* Arbitrary item for symmetry */}
+          <div className='w-6'></div>
         </div>
-
-        {/* Arbitrary item for symmetry */}
-        <div></div>
+        {/* Input unit */}
+        <span className='absolute sm:bottom-14 right-4 sm:right-5 bottom-12 font-medium'>
+          Meters
+        </span>
+        {/* Input */}
+        <Input
+          value={readOnly ? calculation.value : value}
+          onChange={onChange}
+          className={`bg-secondary w-full h-full outline-none text-right sm:rounded-lg !pt-20 p-3 sm:!pt-20 sm:p-5 font-bold text-3xl ${
+            readOnly ? 'rounded-none' : 'rounded-lg'
+          }`}
+          placeholder='0'
+          readOnly={readOnly}
+        />
       </div>
-      {/* Input unit */}
-      <span className='absolute sm:bottom-14 right-4 sm:right-5 bottom-12 font-medium'>
-        Meters
-      </span>
-      {/* Input */}
-      <Input
-        value={value}
-        onChange={onChange}
-        className={`bg-secondary w-full h-full outline-none text-right sm:rounded-lg !pt-20 p-3 sm:!pt-20 sm:p-5 font-bold text-3xl ${
-          readOnly ? 'rounded-none' : 'rounded-lg'
-        }`}
-        placeholder='0'
-        readOnly={readOnly}
-      />
-    </div>
+      {!readOnly && (
+        <Button className='w-full font-bold' onClick={calculateHandler}>
+          CALC
+        </Button>
+      )}
+    </>
   );
 };
 
--- src/routes/Calc.tsx
+++ src/routes/Calc.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { observer } from 'mobx-react';
 
 import CalcInput from '@/lib/components/CalcInput';
 import Parameter from '@/lib/components/Parameter';
@@ -9,11 +10,27 @@ import {
   AccordionTrigger,
 } from '@/components/ui/accordion';
 import { Dialog, DialogContent } from '@/components/ui/dialog';
-import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
+import { calculation } from '@/lib/stores';
 
-const Calc = () => {
+const Calc = observer(() => {
   const [open, setOpen] = useState(false);
+  const [tab, setTab] = useState<'BNS32' | 'BNS22' | 'SMA' | 'Base'>('Base');
+
+  const parameterChangeHandler = (value: number, parameters: any[]) => {
+    if (parameters.length < 3) return;
+    const [layer, group, parameter] = parameters;
+    calculation.parameters.setParameter(layer, group, parameter, value);
+  };
+
+  const limestonePercentageChangeHandler = (
+    value: number,
+    parameters: any[]
+  ) => {
+    if (parameters.length < 2) return;
+    const [layer, index] = parameters;
+    calculation.parameters.setLimestonePercentage(layer, index, value);
+  };
 
   return (
     <div className='w-full max-w-[1000px]'>
@@ -22,63 +39,167 @@ const Calc = () => {
       </div>
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent className='p-3 border-0 bg-transparent'>
-          <div className='rounded-lg'>
-            <CalcInput />
-          </div>
-          <Button className='w-full font-bold'>CALC</Button>
+          <CalcInput onClick={() => setOpen(!open)} />
         </DialogContent>
       </Dialog>
 
       <div className='p-3 sm:px-0'>
-        <div className='flex gap-3 overflow-auto'>
-          <Badge variant='secondary' className='rounded-full cursor-pointer'>
-            0/31
-          </Badge>
-          <Badge className='rounded-full cursor-pointer'>BNS32</Badge>
-          <Badge variant='secondary' className='rounded-full cursor-pointer'>
-            BNS22
-          </Badge>
-          <Badge variant='secondary' className='rounded-full cursor-pointer'>
-            SMA
-          </Badge>
+        <div className='flex gap-3 overflow-auto no-scrollbar'>
+          {['Base', 'BNS32', 'BNS22', 'SMA'].map((item) => (
+            <Badge
+              onClick={() => setTab(item as any)}
+              variant={tab === item ? 'default' : 'secondary'}
+              className='cursor-pointer rounded-full'
+            >
+              {item}
+            </Badge>
+          ))}
         </div>
         <Accordion type='multiple' className='w-full'>
-          <AccordionItem value='item-1'>
+          <AccordionItem value='general'>
             <AccordionTrigger>General</AccordionTrigger>
             <AccordionContent>
               <div className='grid grid-cols-2 sm:grid-cols-4 gap-3 md:gap-4 lg:gap-5'>
-                <Parameter />
-                <Parameter />
-                <Parameter />
-                <Parameter readOnly />
+                {Object.keys(calculation.parameters[tab].general).map(
+                  (parameter) => {
+                    const p = parameter as
+                      | 'thickness'
+                      | 'width'
+                      | 'bulkDensity';
+                    return (
+                      <Parameter
+                        key={`${tab}-general-${parameter}`}
+                        layer={tab}
+                        group='general'
+                        name={p}
+                        value={calculation.parameters[tab].general[p].value}
+                        unit={calculation.parameters[tab].general[p].unit}
+                        onChange={parameterChangeHandler}
+                      />
+                    );
+                  }
+                )}
+                <Parameter
+                  key={`${tab}-general-total`}
+                  computed={
+                    tab === 'Base'
+                      ? calculation.totalBaseWeight
+                      : tab === 'BNS32'
+                      ? calculation.totalBNS32Weight
+                      : tab === 'BNS22'
+                      ? calculation.totalBNS22Weight
+                      : calculation.totalSMAWeight
+                  }
+                  name={
+                    calculation.parameters.type === 'length'
+                      ? 'weight'
+                      : 'length'
+                  }
+                  unit={calculation.parameters.unit === 'm' ? 't' : 'm'}
+                  readOnly
+                />
               </div>
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value='item-2'>
-            <AccordionTrigger>Bitumen</AccordionTrigger>
-            <AccordionContent>
-              <div className='grid grid-cols-2 gap-3 md:gap-4 lg:gap-5'>
-                <Parameter />
-                <Parameter readOnly />
-              </div>
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value='item-3'>
-            <AccordionTrigger>Limestone</AccordionTrigger>
-            <AccordionContent>
-              <div className='grid grid-cols-2 sm:grid-cols-3 gap-3 md:gap-4 lg:gap-5'>
-                <Parameter />
-                <Parameter />
-                <div className='col-span-2 sm:col-span-1'>
-                  <Parameter readOnly />
+          {['BNS32', 'BNS22', 'SMA'].includes(tab) && (
+            <AccordionItem value='bitumen'>
+              <AccordionTrigger>Bitumen</AccordionTrigger>
+              <AccordionContent>
+                <div className='grid grid-cols-2 gap-3 md:gap-4 lg:gap-5'>
+                  <Parameter
+                    key={`${tab}-bitumen-fraction`}
+                    layer={tab}
+                    group='bitumen'
+                    name='fraction'
+                    // @ts-ignore
+                    value={calculation.parameters[tab].bitumen.fraction.value}
+                    // @ts-ignore
+                    unit={calculation.parameters[tab].bitumen.fraction.unit}
+                    onChange={parameterChangeHandler}
+                  />
+                  <Parameter
+                    key={`${tab}-bitumen-total`}
+                    computed={
+                      tab === 'BNS32'
+                        ? calculation.totalBNS32BitumenWeight
+                        : tab === 'BNS22'
+                        ? calculation.totalBNS22BitumenWeight
+                        : calculation.totalSMABitumenWeight
+                    }
+                    name={
+                      calculation.parameters.type === 'length'
+                        ? 'weight'
+                        : 'length'
+                    }
+                    unit={calculation.parameters.unit === 'm' ? 't' : 'm'}
+                    readOnly
+                  />
                 </div>
-              </div>
-            </AccordionContent>
-          </AccordionItem>
+              </AccordionContent>
+            </AccordionItem>
+          )}
+          {'limestone' in calculation.parameters[tab] && (
+            <AccordionItem value='limestone'>
+              <AccordionTrigger>Limestone</AccordionTrigger>
+              <AccordionContent className='grid gap-3 md:gap-4 lg:gap-5'>
+                {
+                  // @ts-ignore
+                  calculation.parameters[tab].limestone.map(
+                    // @ts-ignore
+                    (item, index) => (
+                      <div className='grid grid-cols-2 sm:grid-cols-3 gap-3 md:gap-4 lg:gap-5'>
+                        <Parameter
+                          key={`${tab}-limestone-${index}-thickness`}
+                          name='thickness'
+                          value={item.thickness.value}
+                          unit={item.thickness.unit}
+                          disabled
+                        />
+                        <Parameter
+                          key={`${tab}-limestone-${index}-percentage`}
+                          layer={tab}
+                          group='limestone'
+                          name='percentage'
+                          parameters={[tab, index]}
+                          value={item.percentage.value}
+                          unit={item.percentage.unit}
+                          onChange={limestonePercentageChangeHandler}
+                        />
+                        <div className='col-span-2 sm:col-span-1'>
+                          <Parameter
+                            key={`${tab}-limestone-${index}-total`}
+                            computed={
+                              tab === 'BNS32'
+                                ? calculation.totalBNS32LimestoneWeight[index]
+                                : calculation.totalBNS22LimestoneWeight[index]
+                            }
+                            value={
+                              // @ts-ignore
+                              calculation.parameters[tab].limestone[index]
+                                .percentage.value
+                            }
+                            name={
+                              calculation.parameters.type === 'length'
+                                ? 'weight'
+                                : 'length'
+                            }
+                            unit={
+                              calculation.parameters.unit === 'm' ? 't' : 'm'
+                            }
+                            readOnly
+                          />
+                        </div>
+                      </div>
+                    )
+                  )
+                }
+              </AccordionContent>
+            </AccordionItem>
+          )}
         </Accordion>
       </div>
     </div>
   );
-};
+});
 
 export default Calc;
