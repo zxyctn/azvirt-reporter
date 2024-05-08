27be84b5b3cd9ffe28b86085991ad24345155106
Ibrahim Mammadov <zxyctn@gmail.com>
2024-05-08 20:31:38 +0200

    Resize config icon

--- src/lib/components/CalcInput.tsx
+++ src/lib/components/CalcInput.tsx
@@ -62,14 +62,14 @@ const CalcInput = ({
       <div className='relative'>
         <div className='absolute w-full top-3 flex justify-between h-min px-3'>
           {/* Settings menu */}
-          <div className='w-6'>
+          <div className='w-7'>
             <DropdownMenu>
               <DropdownMenuTrigger asChild className=''>
                 <Button
                   variant='ghost'
-                  className='rounded-full p-0 m-0 w-6 h-6'
+                  className='rounded-full p-0 m-0 w-7 h-7'
                 >
-                  <DotsVerticalIcon className='w-3 h-3' />
+                  <DotsVerticalIcon className='w-5 h-5' />
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent className='right:0 absolute left-auto top-full w-auto'>
@@ -83,7 +83,7 @@ const CalcInput = ({
             </DropdownMenu>
           </div>
           {/* Input type menu */}
-          <div className='-mt-0.5'>
+          <div className=''>
             <DropdownMenu>
               <DropdownMenuTrigger>
                 <Badge className='cursor-pointer'>
@@ -102,7 +102,7 @@ const CalcInput = ({
           </div>
 
           {/* Arbitrary item for symmetry */}
-          <div className='w-6'></div>
+          <div className='w-7'></div>
         </div>
         {/* Input unit */}
         <span className='absolute right-4 sm:right-5 bottom-20 font-medium'>
